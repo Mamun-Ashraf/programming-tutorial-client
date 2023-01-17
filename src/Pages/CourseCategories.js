4a62029b7b0d0130fbe2752ee1023f6e67a7dc24
Mamun-Ashrsf <ashrafdkia@gmail.com>
2023-01-17 08:15:22 +0600

    Organized pages

---
+++ src/Pages/CourseCategories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CourseCategories = () => {
+    return (
+        <div>
+            <h2>This is course categories</h2>
+        </div>
+    );
+};
+
+export default CourseCategories;
